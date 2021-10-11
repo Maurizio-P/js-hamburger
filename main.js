790cd8577147db67484c50a71d2fa7cb0dec887f
Maurizio-P <mauriziopignotti@gmail.com>
2021-10-11 15:55:45 +0200

    add js

---
+++ main.js
@@ -0,0 +1,13 @@
+document.querySelector(".header-left").innerHTML = "<img src=http://boolean.careers/images/misc/logo.png>"
+
+const hamburgerIcon = document.querySelector(".header-right>a")
+const hamburgerMenu = document.querySelector(".hamburger-menu")
+const closeIcon = document.querySelector(".hamburger-menu>a")
+
+hamburgerIcon.addEventListener("click", function(){
+    hamburgerMenu.style.display = "block"
+})
+
+closeIcon.addEventListener("click", function(){
+    hamburgerMenu.style.display = "none"
+})
